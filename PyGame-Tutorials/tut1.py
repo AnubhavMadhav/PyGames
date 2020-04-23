57207ba327121a749180f31a8baddb4c7376cba0
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-23 17:23:59 +0530

    tut1 done

---
+++ PyGame-Tutorials/tut1.py
@@ -0,0 +1,5 @@
+import pygame
+x = pygame.init()
+# print(x)              # All 6 pygame modules succesfully imported
+
+gameWindow = pygame.display.set_mode((1200,500))                    # Game Window of 1200x500
