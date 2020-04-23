458f48f5e80d7b2f9a934945dd7e6f51c9dc3b84
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-23 17:45:11 +0530

    tut4 handling events done

---
+++ PyGame-Tutorials/tut4.py
@@ -0,0 +1,28 @@
+'''
+Handling Events in PyGame
+'''
+
+
+
+import pygame
+pygame.init()
+# print(x)              # All 6 pygame modules succesfully imported
+
+# Creating Game Window
+gameWindow = pygame.display.set_mode((1200,500))                    # Game Window of 1200x500
+pygame.display.set_caption("My First Game")             # Title of the Game, which appears at the top of the window
+
+# Game Specific Variables
+exit_game = False
+game_over = False
+
+
+# Creating a Game Loop
+
+while not exit_game:            # This is an infinite loop for now
+    for event in pygame.event.get():                # This gets all the events which a user can perform in a game, like mouse hover, mouse click, pressing a certain key etc.
+        print(event)
+
+pygame.quit()
+quit()
+
