4c397bbd8edfe52bb67f5be611d9b5d14756ad6f
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-23 17:36:08 +0530

    tut3 infinite game loop done

---
+++ PyGame-Tutorials/tut3.py
@@ -0,0 +1,20 @@
+import pygame
+pygame.init()
+# print(x)              # All 6 pygame modules succesfully imported
+
+# Creating Game Window
+gameWindow = pygame.display.set_mode((1200,500))                    # Game Window of 1200x500
+pygame.display.set_caption("My First Game")             # Title of the Game, which appears at the top of the window
+
+# Game Specific Variables
+exit_game = False
+game_over = False
+
+
+# Creating a Game Loop
+
+while not exit_game:            # This is an infinite loop for now
+    pass
+
+pygame.quit()
+quit()
